e95d745362a2a5a0888cca0ea1c3649f75b637bb
Tim Basten <tbasten@gmail.com>
2024-04-10 01:06:54 +0800

    Sounds

--- Scenes/Prefabs/campfire/campfire.gd
+++ Scenes/Prefabs/campfire/campfire.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+@onready var audio_stream_player_2d = $AudioStreamPlayer2D
+@onready var rayCast2D = $RayCast2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,8 +17,10 @@ func _process(delta):
 func _on_campfire_effect_area_body_entered(body):
 	if body.has_method("warm_self"):
 		body.call("warm_self")
-
+		audio_stream_player_2d.play()
 
 func _on_campfire_effect_area_body_exited(body):
 	if body.has_method("cool_self"):
 		body.call("cool_self")
+		audio_stream_player_2d.stop()
+
